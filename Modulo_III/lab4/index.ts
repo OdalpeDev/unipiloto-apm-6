64b8a6a9e7c420cada5a661108d379d0d6004250
odalpe <oscaralvarezpetro03@gmail.com>
2017-08-12 13:02:36 -0500

    clase 12/07/2017 v2

---
+++ Modulo_III/lab4/index.ts
@@ -0,0 +1,11 @@
+let a:string = 'variable';
+let b:number = 5;
+let c:boolean = true;
+let d:Array<string> = ['a','b','c'];
+let e:any = {};
+
+console.log('a = ' + a);
+console.log('b = ' + b);
+console.log('c = ' + c);
+console.log('d = ' + d);
+console.log('e = ' + e);
