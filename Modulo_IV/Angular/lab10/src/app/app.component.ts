7f0082b38364d87d82c2b7f7936f0f4e541791b3
odalpe90 <oscaralvarezpetro03@gmail.com>
2017-08-25 21:54:20 -0500

    lab10

--- Modulo_IV/Angular/lab10/src/app/app.component.ts
+++ Modulo_IV/Angular/lab10/src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { Person } from '../models/person';
+
+export class Student extends Person {
+  classes: Array<string>;
+  grade: number;
+  group: string;
+
+  constructor(id: number, age: number, name: string, lastname: string, classes: Array<string>, grade: number, group: string) {
+    super(id, age, name, lastname);
+    this.classes = classes;
+    this.grade = grade;
+    this.group = group;
+  }
+ 
+}
 
 @Component({
   selector: 'app-root',
